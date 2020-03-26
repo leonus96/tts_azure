6768be44e7a6d6210ed960c2e7b576e1d711ce44
Joseph Leon <leonus96@gmail.com>
2020-03-26 17:41:16 -0500

    Token reload in 9 minutes. Speech to text.

---
+++ example/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:tts_azure/tts_azure.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TTSAzure',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'TTSAzure'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TTSAzure _ttsazure;
+  TextEditingController _controller;
+  String _lang = 'es-ES';
+  String _shortName = 'es-ES-Pablo-Apollo';
+  
+  @override
+  void initState() {
+    _ttsazure = TTSAzure("YOUR_SUSCRIPTION_ID", 'YOUR_REGION_IDENTIFIER');
+    _controller = TextEditingController();
+    _controller.text = "Hello World!";
+    super.initState();
+  }
+
+  void _play() {
+    _ttsazure.play(_controller.text, _lang, _shortName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: TextField(
+                maxLines: 10,
+                controller: _controller,
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _play,
+        tooltip: 'Speech to Text',
+        child: Icon(Icons.play_arrow),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/tts_azure.dart
@@ -0,0 +1,82 @@
+library tts_azure;
+
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+import 'package:audioplayer/audioplayer.dart';
+import 'package:xml/xml.dart' as xml;
+import 'package:http/http.dart' as http;
+
+
+class TTSAzure {
+  AudioPlayer _audioPlugin;
+  String _regionIdentifier;
+  String _subscriptionKey;
+  String _accessToken;
+  DateTime _accessTokenTimestamp;
+
+  TTSAzure(String subscriptionKey, String regionIdentifier) {
+    this._regionIdentifier = regionIdentifier;
+    this._subscriptionKey = subscriptionKey;
+    _audioPlugin = AudioPlayer();
+    _updateAccessToken();
+  }
+
+  Future<String> _getAccessToken() async {
+    if(_accessTokenTimestamp == null
+    || (_accessTokenTimestamp.difference(DateTime.now()).inMinutes >= 9)) {
+      _accessTokenTimestamp = DateTime.now();
+      await _updateAccessToken();
+    }
+    return _accessToken;
+  }
+
+  Future<void> _updateAccessToken() async {
+    String url = "https://$_regionIdentifier.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+    Map<String, String> headers = {
+      'Ocp-Apim-Subscription-Key': _subscriptionKey
+    };
+    http.Response response =  await http.post(url, headers: headers);
+    _accessToken = response.body;
+  }
+
+
+  Future<void> play(String text, String lang, String shortName) async {
+    xml.XmlBuilder builder = xml.XmlBuilder();
+    builder.element('speak', nest: () {
+      builder.attribute('version', '1.0');
+      builder.attribute('xml:lang', lang);
+      builder.element('voice', nest: () {
+        builder.attribute('xml:lang', lang);
+        builder.attribute('name', shortName);
+        builder.text(text);
+      });
+    });
+
+    String body = builder.build().toXmlString();
+
+    String url = "https://$_regionIdentifier.tts.speech.microsoft.com/cognitiveservices/v1";
+    Map<String, String> headers = {
+      'Authorization': 'Bearer ' + await _getAccessToken(),
+      'cache-control': 'no-cache',
+      'User-Agent': 'TTSPackage',
+      'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+      'Content-Type': 'application/ssml+xml'
+    };
+
+    http.Response request = await http.post(url, headers: headers, body: body);
+  
+    Uint8List bytes = request.bodyBytes;
+
+    final dir = await getApplicationDocumentsDirectory();
+    final file = new File('${dir.path}/audio.mp3');
+
+    await file.writeAsBytes(bytes);
+    if (await file.exists()) {
+      _audioPlugin.play(file.path, isLocal: true);
+    }
+  }
+}
+
+
+
