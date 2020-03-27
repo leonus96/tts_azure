3477c2953ea8a2341b0a0701a5c87d59476e0314
Joseph Leon <leonus96@gmail.com>
2020-03-27 08:19:40 -0500

    delete default comments

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:tts_azure/tts_azure.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,16 +18,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -75,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _play,
         tooltip: 'Speech to Text',
         child: Icon(Icons.play_arrow),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
